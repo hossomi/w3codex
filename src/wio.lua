ab83d26ecff9211e58c83399fef94f6993da1ec6
Marcelo Hossomi <marcelo.hossomi@gmail.com>
2019-07-28 04:42:28 +0000

    Issue #1: Removed StringReader.

--- src/wio.lua
+++ src/wio.lua
@@ -17,87 +17,10 @@ local function size(file)
   return size
 end
 
-local function withReadFormats(reader)
-  reader.read = function(self, arg, options)
-    if type(arg) == 'number' then
-      return self:readBytes(arg, options)
-    elseif type(arg) == 'string' then
-      return self:readUntil(arg, options)
-    end
-  end
-  reader.bytes = function(self, n)
-    return self:read(n)
-  end
-  reader.string = function(self)
-    return self:read('\0')
-  end
-  reader.int = function(self)
-    return unpack(self:read(4), '<I4')
-  end
-  reader.short = function(self)
-    return unpack(self:read(2), '<I2')
-  end
-  reader.real = function(self)
-    return unpack(self:read(4), '<f')
-  end
-  return reader
-end
-
-local function withWriteFormats(writer)
-  writer.bytes = function(self, data)
-    self:write(data)
-  end
-  writer.string = function(self, data)
-    self:write(data .. '\0')
-  end
-  writer.int = function(self, data)
-    self:write(pack(data, '<I4'))
-  end
-  writer.short = function(self, data)
-    self:write(pack(data, '<I2'))
-  end
-  writer.real = function(self, data)
-    self:write(pack(data, '<f'))
-  end
-  return writer
-end
-
-local function StringReader(data)
-  return {
-    _cursor = 1,
-
-    _remaining = function(self)
-      return #data - self._cursor + 1
-    end,
-
-    readBytes = function(self, n)
-      if self:_remaining() >= n then
-        self._cursor = self._cursor + n
-        return data:sub(self._cursor - n, self._cursor - 1)
-      end
-    end,
-
-    readUntil = function(self, pattern, options)
-      local left, right = data:find(pattern, self._cursor)
-      if left then
-        local from = self._cursor
-        self._cursor = right + 1
-        if options and options.inclusive then
-          return data:sub(from, right)
-        else
-          return data:sub(from, left - 1)
-        end
-      end
-    end,
-
-    skip = function(self, n)
-      self._cursor = math.min(self._cursor + n, #data + 1)
-    end
-  }
-end
-
 local function FileReader(file, bsize)
   return {
+    _file = file,
+
     _buffer = file:read(bsize) or '',
 
     _cursor = 1,
@@ -109,7 +32,7 @@ local function FileReader(file, bsize)
     _fsize = size(file),
 
     _fremaining = function(self)
-      return self._fsize - file:seek() + 1
+      return self._fsize - self._file:seek() + 1
     end,
 
     readBytes = function(self, n)
@@ -118,8 +41,8 @@ local function FileReader(file, bsize)
         return self._buffer:sub(self._cursor - n, self._cursor - 1)
       elseif self:_fremaining() + self:_bremaining() >= n then
         local load = n - self:_bremaining()
-        local data = self._buffer:sub(self._cursor) .. file:read(load)
-        self._buffer = file:read(math.ceil(load / bsize) * bsize - load)
+        local data = self._buffer:sub(self._cursor) .. self._file:read(load)
+        self._buffer = self._file:read(math.ceil(load / bsize) * bsize - load)
         self._cursor = 1
         return data
       end
@@ -139,9 +62,9 @@ local function FileReader(file, bsize)
       end
 
       local data = self._buffer
-      local current = file:seek()
-      while file:seek() < self._fsize do
-        self._buffer = file:read(bsize) or ''
+      local current = self._file:seek()
+      while self._file:seek() < self._fsize do
+        self._buffer = self._file:read(bsize) or ''
         left, right = self._buffer:find(pattern)
         if left then
           if options and options.inclusive then
@@ -154,7 +77,7 @@ local function FileReader(file, bsize)
         end
         data = data .. self._buffer
       end
-      file:seek('set', current)
+      self._file:seek('set', current)
     end,
 
     skip = function(self, n)
@@ -162,51 +85,100 @@ local function FileReader(file, bsize)
         self._cursor = self._cursor + n
       elseif self:_fremaining() + self:_bremaining() >= n then
         local load = n - self:_bremaining()
-        file:seek('set', file:seek() + load)
-        self._buffer = file:read(math.ceil(load / bsize) * bsize - load)
+        self._file:seek('set', self._file:seek() + load)
+        self._buffer = self._file:read(math.ceil(load / bsize) * bsize - load)
         self._cursor = 1
       else
         self._buffer = ''
         self._cursor = 1
-        file:seek('end')
+        self._file:seek('end')
       end
     end,
 
     close = function(self)
-      file:close()
+      self._file:close()
+    end,
+
+    read = function(self, arg, options)
+      if type(arg) == 'number' then
+        return self:readBytes(arg, options)
+      elseif type(arg) == 'string' then
+        return self:readUntil(arg, options)
+      end
+    end,
+
+    bytes = function(self, n)
+      return self:read(n)
+    end,
+
+    string = function(self)
+      return self:read('\0')
+    end,
+
+    int = function(self)
+      return unpack(self:read(4), '<I4')
+    end,
+
+    short = function(self)
+      return unpack(self:read(2), '<I2')
+    end,
+
+    real = function(self)
+      return unpack(self:read(4), '<f')
     end
   }
 end
 
 local function FileWriter(file, bsize)
   return {
+    _file = file,
+
     _buffer = '',
 
     write = function(self, data)
       self._buffer = self._buffer .. data
       if #self._buffer >= bsize then
-        file:write(self._buffer)
+        self._file:write(self._buffer)
         self._buffer = ''
       end
     end,
 
     close = function(self)
       if #self._buffer > 0 then
-        file:write(self._buffer)
+        self._file:write(self._buffer)
       end
-      file:close()
+      self._file:close()
+    end,
+
+    bytes = function(self, data)
+      self:write(data)
+    end,
+
+    string = function(self, data)
+      self:write(data .. '\0')
+    end,
+
+    int = function(self, data)
+      self:write(pack(data, '<I4'))
+    end,
+
+    short = function(self, data)
+      self:write(pack(data, '<I2'))
+    end,
+
+    real = function(self, data)
+      self:write(pack(data, '<f'))
     end
   }
 end
 
 return {
-  StringReader = function(data)
-    return withReadFormats(StringReader(data))
+  FileReader = function(path, bsize)
+    local file = assert(io.open(path, 'rb'))
+    return FileReader(file, bsize or 2 ^ 13)
   end,
-  FileReader = function(file, bsize)
-    return withReadFormats(FileReader(file, bsize or 2 ^ 13))
-  end,
-  FileWriter = function(file, bsize)
-    return withWriteFormats(FileWriter(file, bsize or 2 ^ 2 ^ 10))
+  FileWriter = function(path, bsize)
+    local file = assert(io.open(path, 'wb'))
+    return FileWriter(file, bsize or 2 ^ 2 ^ 10)
   end
 }
--- src/wio.test.lua
+++ src/wio.test.lua
@@ -1,240 +1,184 @@
 local wio = require 'wio'
 local util = require 'util'
 
-local function testReadFormats(Reader)
-  describe('bytes(n)', function()
-    it('should return data if enough data', function()
-      local reader = Reader('test/wio/bytes.bin')
-      assert.are.equals(reader:bytes(4), 'MARC')
-    end)
-
-    it('should return data if just enough data', function()
-      local reader = Reader('test/wio/bytes.bin')
-      assert.are.equals(reader:bytes(7), 'MARCELO')
-    end)
-
-    it('should return nil if not enough data', function()
-      local reader = Reader('test/wio/bytes.bin')
-      assert.is_nil(reader:bytes(50))
-    end)
-  end)
-
-  describe('string()', function()
-    it('should return data up to terminator', function()
-      local reader = Reader('test/wio/string.bin')
-      assert.are.equals(reader:string(), 'MARCELO HOSSOMI')
-    end)
-
-    it('should return nil if no terminator', function()
-      local reader = Reader('test/wio/bytes.bin')
-      assert.is_nil(reader:string())
-    end)
-  end)
-
-  describe('int()', function()
-    it('should read little endian', function()
-      local reader = Reader('test/wio/integer.bin')
-      assert.are.equals(reader:int(), 1)
-    end)
-
-    it('should return nil if no more data', function()
-      local reader = Reader('test/wio/empty.bin')
-      assert.are.is_nil(reader:int())
-    end)
-  end)
-
-  describe('short()', function()
-    it('should read little endian', function()
-      local reader = Reader('test/wio/short.bin')
-      assert.are.equals(reader:short(), 1)
-    end)
-
-    it('should return nil if no more data', function()
-      local reader = Reader('test/wio/empty.bin')
-      assert.are.is_nil(reader:short())
-    end)
-  end)
-
-  describe('real()', function()
-    it('should read little endian', function()
-      local reader = Reader('test/wio/real.bin')
-      assert.is_true(math.abs(reader:real() - 1.23) < 1E-5)
-    end)
-
-    it('should return nil if no more data', function()
-      local reader = Reader('test/wio/empty.bin')
-      assert.are.is_nil(reader:real())
-    end)
-  end)
-end
-
-local function testWriteFormats(assert)
-  it('should write bytes', function()
-    writer:bytes('MARCELO HOSSOMI')
-    assert('MARCELO HOSSOMI')
-  end)
-
-  it('should write string', function()
-    writer:string('MARCELO HOSSOMI')
-    assert('MARCELO HOSSOMI' .. '\0')
-  end)
-
-  it('should write integer', function()
-    writer:int(1)
-    assert(util.hexToBin('01000000'))
-  end)
-
-  it('should write short', function()
-    writer:short(1)
-    assert(util.hexToBin('0100'))
-  end)
-
-  it('should write real', function()
-    writer:real(1.23)
-    assert(util.hexToBin('A4709D3F'))
-  end)
-end
-
 describe('WIO', function()
-
-  describe('StringReader', function()
-
-    testReadFormats(function(path)
-      local file = assert(io.open(path, 'rb'))
-      return wio.StringReader(file:read('*all'))
-    end)
-
-    before_each(function()
-      reader = wio.StringReader(util.hexToBin('4D415243454C4F20484F53534F4D49'))
-    end)
-
-    it('should start at 1', function()
-      assert.are.equals(reader._cursor, 1)
-    end)
-
-    describe('readBytes(n)', function()
-      it('should return data and advance', function()
-        assert.are.equals(reader:readBytes(7), 'MARCELO')
-        assert.are.equals(reader._cursor, 8)
-      end)
-
-      it('should return nil and not advance if not enough data', function()
-        assert.is_nil(reader:readBytes(666))
-        assert.are.equals(reader._cursor, 1)
-      end)
-    end)
-
-    describe('readUntil(d)', function()
-      it('should return data without delimiter and advance', function()
-        assert.are.equals(reader:readUntil(' '), 'MARCELO')
-        assert.are.equals(reader._cursor, 9)
-      end)
-
-      it('should return data with delimiter and advance', function()
-        assert.are.equals(reader:readUntil(' ', {inclusive = true}), 'MARCELO ')
-        assert.are.equals(reader._cursor, 9)
-      end)
-
-      it('should return nil and not advance if not found', function()
-        assert.is_nil(reader:readUntil('XABLAU'))
-        assert.are.equals(reader._cursor, 1)
-      end)
-    end)
-
-    describe('skip(n)', function()
-      it('should advance', function()
-        reader:skip(5)
-        assert.are.equals(reader._cursor, 6)
-      end)
-
-      it('should stop at end', function()
-        reader:skip(666)
-        assert.are.equals(reader._cursor, 16)
-      end)
-    end)
-  end)
 
   describe('FileReader', function()
 
-    testReadFormats(function(path)
-      local file = assert(io.open(path, 'rb'))
-      return wio.FileReader(file, 2)
-    end)
-
-    before_each(function()
-      file = assert(io.open('test/wio/bytes.bin', 'rb'))
-      reader = wio.FileReader(file, 2)
-    end)
-
     it('should start at 1', function()
+      local reader = wio.FileReader('test/wio/bytes.bin', 2)
       assert.are.equals(reader._cursor, 1)
-      assert.are.equals(file:seek(), 2)
+      assert.are.equals(reader._file:seek(), 2)
     end)
 
     describe('readBytes(n)', function()
+
       it('should return data and advance', function()
+        local reader = wio.FileReader('test/wio/bytes.bin', 2)
         assert.are.equals(reader:readBytes(7), 'MARCELO')
         assert.are.equals(reader._cursor, 1)
-        assert.are.equals(file:seek(), 8)
+        assert.are.equals(reader._file:seek(), 8)
       end)
 
       it('should return nil and not advance if not enough data', function()
+        local reader = wio.FileReader('test/wio/bytes.bin', 2)
         assert.is_nil(reader:readBytes(666))
         assert.are.equals(reader._cursor, 1)
-        assert.are.equals(file:seek(), 2)
+        assert.are.equals(reader._file:seek(), 2)
       end)
     end)
 
     describe('readUntil(d)', function()
+
       it('should return data without delimiter and advance', function()
+        local reader = wio.FileReader('test/wio/bytes.bin', 2)
         assert.are.equals(reader:readUntil(' '), 'MARCELO')
         assert.are.equals(reader._cursor, 3)
-        assert.are.equals(file:seek(), 8)
+        assert.are.equals(reader._file:seek(), 8)
       end)
 
       it('should return data with delimiter and advance', function()
+        local reader = wio.FileReader('test/wio/bytes.bin', 2)
         assert.are.equals(reader:readUntil(' ', {inclusive = true}), 'MARCELO ')
         assert.are.equals(reader._cursor, 3)
-        assert.are.equals(file:seek(), 8)
+        assert.are.equals(reader._file:seek(), 8)
       end)
 
       it('should return nil and not advance if not found', function()
+        local reader = wio.FileReader('test/wio/bytes.bin', 2)
         assert.is_nil(reader:readUntil('XABLAU'))
         assert.are.equals(reader._cursor, 1)
-        assert.are.equals(file:seek(), 2)
+        assert.are.equals(reader._file:seek(), 2)
       end)
     end)
 
     describe('skip(n)', function()
+
       it('should advance', function()
+        local reader = wio.FileReader('test/wio/bytes.bin', 2)
         reader:skip(5)
         assert.are.equals(reader._cursor, 1)
-        assert.are.equals(file:seek(), 6)
+        assert.are.equals(reader._file:seek(), 6)
       end)
 
       it('should stop at end', function()
+        local reader = wio.FileReader('test/wio/bytes.bin', 2)
         reader:skip(666)
         assert.are.equals(reader._cursor, 1)
-        assert.are.equals(file:seek(), 15)
+        assert.are.equals(reader._file:seek(), 15)
+      end)
+    end)
+
+    describe('bytes(n)', function()
+
+      it('should return data if enough data', function()
+        local reader = wio.FileReader('test/wio/bytes.bin')
+        assert.are.equals(reader:bytes(4), 'MARC')
+      end)
+
+      it('should return data if just enough data', function()
+        local reader = wio.FileReader('test/wio/bytes.bin')
+        assert.are.equals(reader:bytes(7), 'MARCELO')
+      end)
+
+      it('should return nil if not enough data', function()
+        local reader = wio.FileReader('test/wio/bytes.bin')
+        assert.is_nil(reader:bytes(50))
+      end)
+    end)
+
+    describe('string()', function()
+
+      it('should return data up to terminator', function()
+        local reader = wio.FileReader('test/wio/string.bin')
+        assert.are.equals(reader:string(), 'MARCELO HOSSOMI')
+      end)
+
+      it('should return nil if no terminator', function()
+        local reader = wio.FileReader('test/wio/bytes.bin')
+        assert.is_nil(reader:string())
+      end)
+    end)
+
+    describe('int()', function()
+
+      it('should read little endian', function()
+        local reader = wio.FileReader('test/wio/integer.bin')
+        assert.are.equals(reader:int(), 1)
+      end)
+
+      it('should return nil if no more data', function()
+        local reader = wio.FileReader('test/wio/empty.bin')
+        assert.are.is_nil(reader:int())
+      end)
+    end)
+
+    describe('short()', function()
+
+      it('should read little endian', function()
+        local reader = wio.FileReader('test/wio/short.bin')
+        assert.are.equals(reader:short(), 1)
+      end)
+
+      it('should return nil if no more data', function()
+        local reader = wio.FileReader('test/wio/empty.bin')
+        assert.are.is_nil(reader:short())
+      end)
+    end)
+
+    describe('real()', function()
+
+      it('should read little endian', function()
+        local reader = wio.FileReader('test/wio/real.bin')
+        assert.is_true(math.abs(reader:real() - 1.23) < 1E-5)
+      end)
+
+      it('should return nil if no more data', function()
+        local reader = wio.FileReader('test/wio/empty.bin')
+        assert.are.is_nil(reader:real())
       end)
     end)
   end)
 
   describe('FileWriter', function()
 
-    testWriteFormats(function(expected)
-      writer:close()
-      local file = assert(io.open('test/wio/out.bin', 'rb'))
-      assert.are.equals(expected, file:read('*all'))
-    end)
-
     before_each(function()
-      local file = assert(io.open('test/wio/out.bin', 'wb'))
-      writer = wio.FileWriter(file, 4)
+      writer = wio.FileWriter('test/wio/out.bin', 4)
+
+      assert.data = function(expected)
+        writer:close()
+        local file = assert(io.open('test/wio/out.bin', 'rb'))
+        assert.are.equals(expected, file:read('*all'))
+      end
     end)
 
     after_each(function()
       os.remove('test/wio/out.bin')
+    end)
+
+    it('should write bytes', function()
+      writer:bytes('MARCELO HOSSOMI')
+      assert.data('MARCELO HOSSOMI')
+    end)
+
+    it('should write string', function()
+      writer:string('MARCELO HOSSOMI')
+      assert.data('MARCELO HOSSOMI' .. '\0')
+    end)
+
+    it('should write integer', function()
+      writer:int(1)
+      assert.data(util.hexToBin('01000000'))
+    end)
+
+    it('should write short', function()
+      writer:short(1)
+      assert.data(util.hexToBin('0100'))
+    end)
+
+    it('should write real', function()
+      writer:real(1.23)
+      assert.data(util.hexToBin('A4709D3F'))
     end)
   end)
 end)
