abaabb17f18205df6f087757a4c65c25f8400ad0
Marcelo Hossomi <marcelo.hossomi@gmail.com>
2019-08-27 10:58:37 -0300

    Issue #23: Simplified blocks function; renamed to BinFormetter.

--- src/binformatter/init.lua
+++ src/binformatter/init.lua
@@ -1,32 +1,25 @@
 local util = require 'src.util'
 local colors = require 'term.colors'
 
-local function linePrefix(line)
-  return string.format('%08X', line - 1)
+local function breplace(block)
+  return util.format.b2x(block) .. ' '
 end
 
-local function lineBlocks(line, bsize)
-  local blocks = ''
-  for c = 1, #line, bsize do
-    blocks = blocks .. util.format.b2x(line:sub(c, c + bsize - 1)) .. ' '
-  end
-  return blocks
-end
-
-local function BinFormat(columns, bsize)
+local function BinFormatter(columns, bsize)
   return function(data)
     local out = {}
 
     local lsize = columns * bsize
     local bwidth = lsize * 2 + columns - 1
-    local line, blocks
+    local bpattern = '(' .. string.rep('.', bsize) .. ')'
 
+    local line, blocks
     for l = 1, #data, lsize do
       line = data:sub(l, l + lsize - 1)
       table.insert(out, string.format('%08X', l - 1))
       table.insert(out, '  ')
 
-      blocks = lineBlocks(line, bsize)
+      blocks = (line:gsub(bpattern, breplace))
       table.insert(out, blocks)
       table.insert(out, string.rep(' ', bwidth - #blocks + 2))
 
@@ -42,4 +35,4 @@ local data = ''
 for i = 0x00, 0xFF do
   data = data .. string.char(i)
 end
-print(BinFormat(8, 2)(data))
+print(BinFormatter(8, 2)(data))
